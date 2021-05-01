20b616eb9eb3f9860b2690c887252ce86afbc6af
Alena Fatianova <ravemybody@gmail.com>
2021-05-01 19:49:06 +0500

    add new components, install react, work with front side

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {VideoPlayer} from './components/VideoPlayer'
+import {Options} from './components/Options'
+import {Notifications} from './components/Notifications'
+
+
+export const App = () => {
+  return (
+    <div className="App">
+      <nav>
+        <h2>Video Chat</h2>
+      </nav>
+        <VideoPlayer/>
+        <Options/>
+        <Notifications/>
+    </div>
+  );
+}
+
+
