5d62a504e7be3dc4b07f471e1ce67091e34ded91
Alena Fatianova <ravemybody@gmail.com>
2021-05-01 22:27:09 +0500

    add form for video caht

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useContext, useState} from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import {SocketContext} from '../SocketContext'
+import './Options.css'
+
 
 export const Options = ({children}) => {
+    
+    const context = useContext(SocketContext)
+
+    const {me, isCallAccepted, isCallEnded, name, setName, leaveCall, callToUser} = useContext(SocketContext)
+    const [idToCall, setIdToCall] = useState('')
+
+
     return (
-        <div>
+        <div className='container'>
+            <div>
+                <form noValidate autoComplete='off' className='form_class'>
+                    <h5>Account Info</h5>
+                    <label>Name</label>
+                        <input className='input_form' value={name} onChange={(e) => {setName(e.currentTarget.value)}} />
+                        <CopyToClipboard>
+                            <button className='main_btn'></button>
+                        </CopyToClipboard>
+                </form>
+            </div>
             Options
             {children}
         </div>
