8361bffaf0068d330f9006ef85ee0ffa047b3665
Alena Fatianova <ravemybody@gmail.com>
2021-05-06 17:37:22 +0500

    create separate folders for each component

---
+++ client/src/components/Notifications/Notifications.jsx
@@ -0,0 +1,20 @@
+import React, {useContext} from 'react'
+import {SocketContext} from '../../SocketContext'
+import './Notifications.css'
+
+export const Notifications = () => {
+    const {answerCall, isCallAccepted, call} = useContext(SocketContext)
+
+    return (
+        <>
+        {call.isReceivingCall && !isCallAccepted && (
+            <div className="answer_call_block">
+                <h1>{call.name} is calling: </h1>
+                <button onClick={answerCall} type="button" className="answer_call_btn">
+                    Answer
+                </button>
+            </div>
+        )}
+        </>
+    )
+}
